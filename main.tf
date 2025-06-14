ddc9c27cfa07cd93bd986d4935b9fbaffbb45fa3
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-14 17:51:05 +0000

    init commit

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+module "vpc" {
+  source = "./modules/vpc"
+}
+
+module "ec2" {
+  source         = "./modules/ec2"
+  ami_id         = var.ami_id
+  instance_type  = var.instance_type
+  key_name       = var.key_name
+  vpc_id         = module.vpc.vpc_id
+  subnet_ids     = module.vpc.public_subnet_ids
+}
+
+module "alb" {
+  source            = "./modules/alb"
+  vpc_id            = module.vpc.vpc_id
+  subnet_ids        = module.vpc.public_subnet_ids
+  ec2_id            = module.ec2.ec2_id
+  security_group_id = module.ec2.security_group_id
+}
