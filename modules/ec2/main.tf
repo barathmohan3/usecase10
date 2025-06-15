52ee7eed133e7a56a97d62b739480c0df54652cf
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-15 13:54:32 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -41,7 +41,7 @@ resource "aws_instance" "focal_ec2" {
   subnet_id                   = var.subnet_ids[0]
   vpc_security_group_ids      = [aws_security_group.focal_sg.id]
   associate_public_ip_address = true
-  key_name                    = var.key_name
+  key_name                    = aws_key_pair.focalboard_key.key_name
 
   user_data = <<-EOF
               #!/bin/bash
