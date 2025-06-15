850e891aea6a73d733ab55736414fcc119e04421
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-15 13:58:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,7 @@ module "ec2" {
   source         = "./modules/ec2"
   ami_id         = var.ami_id
   instance_type  = var.instance_type
+  key_name = aws_key_pair.focalboard_key.key_name
   vpc_id         = module.vpc.vpc_id
   subnet_ids     = module.vpc.public_subnet_ids
 }
